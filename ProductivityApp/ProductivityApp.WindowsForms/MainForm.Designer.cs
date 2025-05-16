ad95375566439df6cc16e21e2f9c27c73c2def61
herougo <herougo@gmail.com>
2025-05-16 10:49:19 -0400

    refactor: renamed Form1 to MainForm

--- ProductivityApp/ProductivityApp.WindowsForms/MainForm.Designer.cs
+++ ProductivityApp/ProductivityApp.WindowsForms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProductivityApp.WindowsForms
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
