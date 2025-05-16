8d96aac09897b95afcb9097c79552a836d8cfcdc
herougo <herougo@gmail.com>
2025-05-16 19:36:15 -0400

    feat: added DB migrations (again)

---
+++ ProductivityApp/ProductivityApp.Core/Migrations/20250516233535_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductivityApp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ButtonClick",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClickedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ClickType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ButtonClick", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ButtonClick");
+        }
+    }
+}
