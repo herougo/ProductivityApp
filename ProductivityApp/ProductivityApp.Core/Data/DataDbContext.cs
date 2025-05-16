edab3c00fa3317f4332b9d2b291ec626b73fca8a
herougo <herougo@gmail.com>
2025-05-16 14:12:09 -0400

    feat: base DataDbContext and ButtonClicks model

---
+++ ProductivityApp/ProductivityApp.Core/Data/DataDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductivityApp.Core.Data.Models;
+
+namespace ProductivityApp.Core.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public string DbPath { get; }
+
+        public DataDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            string path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "productivity.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source={DbPath}");
+        }
+
+        public DbSet<ButtonClicks> Question { get; set; } = null!;
+    }
+}
---
+++ ProductivityApp/ProductivityApp.Core/Data/Models/ButtonClicks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityApp.Core.Data.Models
+{
+    public class ButtonClicks
+    {
+        public int Id { get; set; }
+    }
+}
